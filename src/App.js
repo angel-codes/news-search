cd40a7e09c82b9490e4077358f2ed63890ef1624
Angel Martinez <angel.codes139@gmail.com>
2020-08-19 20:53:02 -0500

    Cleaning project

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
 import React from 'react';
-import tw from 'twin.macro';
-
-// Components
-import Content from './components/Content';
-
-// Styled Components
-const Container = tw.div`min-h-screen flex flex-col items-center justify-center bg-gray-800 text-center`;
 
 function App() {
   return (
-    <Container>
-      <Content/>
-    </Container>    
+    <h1>Hello World</h1>
   );
 }
 
